6cfffc3a8bf5c68643f36c37481bd97d8bc0318c
ronialchemist <ronialchemist@gmail.com>
2021-07-23 16:58:08 -0300

    fetch books info

--- index.js
+++ index.js
@@ -19,10 +19,33 @@ const main = async () => {
 		return chunkedArray;
 	}
 
+  const downloadFile = async (url, path) => {
+    const writer = fs.createWriteStream(path);
+
+    const response = await axios({
+      url,
+      method: 'GET',
+      responseType: 'stream'
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+  }
+
 	const fetchHTML = async url => {
 		try {
-			const { data } = await axios.get(url);
-			return data;
+			const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'arraybuffer',
+        reponseEncoding: 'binary'
+      });
+
+			return response;
 		} catch (e) {
 			return e;
 		}
@@ -40,27 +63,76 @@ const main = async () => {
 		return isbnAndQty;	
 	}
 
-	const getLinks = async () => {
+	const getBooksInfo = async () => {
 		chunk(getIsbnAndQty('./Barcode.html'), 3).forEach(async isbnAndQty => {
 			try {
-				const data = await axios.get();
-				console.log(data.status);
+        const response = await fetchHTML(`https://www.bookfinder.com/search/?isbn=${isbnAndQty[1]}&mode=isbn&st=sr&ac=qr`);
+        console.log(response.data);
+        const $ = cheerio.load(response.data.toString('latin1'));
+
+        console.log('test');
+
+        if ($('p[align=center] big').text()) {
+          console.log(`${isbnAndQty[1]} Não foi encontrado`);
+        } else {
+          // get image
+          if ($('img[id=coverImage]').html()) {
+            const url = $('img[id=coverImage]')[0].attribs.src;
+            const filenameExtension = url.split('.')[url.split('.').length - 1];
+	          const filename = `${__dirname}/${Date.now().toString()}.${filenameExtension}`;
+	          await downloadFile(url, filename);
+          } else {
+            console.log('imagem não encontrada');
+          }
+
+          // get title
+          if ($('span[id=describe-isbn-title]').text()) {
+            const title = $('span[id=describe-isbn-title]').text();
+          } else {
+            console.log('título não encontrado');
+          }
+
+          // get author
+          if ($('span[itemprop=author]').text()) {
+            const author = $('span[itemprop=author]').text();
+          } else {
+            console.log('autor não encontrado');
+          }
+
+          // get publisher
+          if ($('span[itemprop=publisher]').text()) {
+            const publisher = $('span[itemprop=publisher]').text();
+          } else {
+            console.log('editora não encontrada');
+          }
+
+          // get sumary
+          if ($('div[id=bookSummary]').text()) {
+            const summary = $('div[id=bookSummary]').text();
+          } else {
+            console.log('sinopse não encontrada');
+          }
+
+          // get rating average
+          if ($('div[class=rating] span:first').text()) {
+            const rating = $('div[class=rating] span:first').html().split(' ')[0];
+          } else {
+            console.log('avaliação não encontrada');
+          }
+        }
+
+        console.log(title);
+        console.log(author);
+        console.log(publisher);
+        console.log(summary);
+        console.log(rating);
 			} catch (e) {
-				console.log(`${isbnAndQty[1]} Não foi encontrado.`);
+				console.log(e);
 			}
 		});
 	}
 
-	getLinks();
-
-	/* const data = await fetchHTML('https://www.starwars.com/news');
-
-	const $ = cheerio.load(data);
-
-	$('.news-articles li')
-	.each((index, element) => {
-		console.log($(element).find('h2').text().trim());
-	}); */
+  getBooksInfo();
 }
 
 main();
