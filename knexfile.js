989499ce2e9814160084627af01e767a32b3b80f
ronialchemist <ronialchemist@gmail.com>
2021-07-26 11:16:50 -0300

    selector bug fix

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'root',
+      password: 'toor',
+      database: 'cpm_library'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
